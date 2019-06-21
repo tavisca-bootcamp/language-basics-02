3cc1f450547bfb3eaa81fb8d65ad584895ddec13
tavisca-dshrivastav <dshrivastav@tavisca.in>
2019-06-21 13:07:06 +0530

    Comment added in code

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -30,21 +30,31 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 {
                     if(exactPostTime[i]==exactPostTime[j])
                     {
+                        //checking for different msgs.. if there is two different msgs for same time then 
+                        // return "impossible" 
                         if (showPostTime[i] != showPostTime[j])
                             return "impossible";
                     }
                 }
+                //parsing exact post time to a timespan so that we can get hour, minutes and seconds from exactposttime and do calculations
                 TimeSpan timeSpan = TimeSpan.Parse(exactPostTime[i]);
+                //checking for seconds msg
                 if (showPostTime[i].Contains("few"))
                 {
+                    //creating time 
                     TimeSpan time = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    //adding time to current time list.
                     currTime.Add(time);
                 }
+                //checking for minutes msg
                 else if(showPostTime[i].Contains("minutes"))
                 {
+                    //getting number of minutes from minutes msg
                     string minute = showPostTime[i].Split(' ')[0];
+                    //adding minutes in timespan
                     timeSpan = timeSpan.Add(TimeSpan.FromMinutes(double.Parse(minute)));
                     TimeSpan time = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    //adding time to current time list
                     currTime.Add(time);
                 }
                 else
@@ -56,7 +66,9 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 }
                 
             }
+            //sorting currTime
             currTime.Sort();
+            //return last value from currTime list.
             return currTime[currTime.Count - 1].ToString();
             throw new NotImplementedException();
         }
