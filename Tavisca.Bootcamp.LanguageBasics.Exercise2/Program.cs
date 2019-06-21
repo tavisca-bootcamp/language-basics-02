3e586d77eaf85db0a5b2abef8feddcbeb6d6ee0f
tavisca-ysant <ysant@tavisca.in>
2019-06-21 12:25:06 +0530

    Solved language basics - 02

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -21,10 +21,74 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
+        public static string process(string ExactPostTime, string ShowPostTime){
+                    int index = ExactPostTime.LastIndexOf(':');
+                   string temp = ExactPostTime.Substring(index+1);
+                   //get seconds, hours and mins
+                   int sec = Convert.ToInt32(temp);
+                   int min = Convert.ToInt32(ExactPostTime.Substring(ExactPostTime.IndexOf(':')+1,2));
+                   int hrs = Convert.ToInt32(ExactPostTime.Substring(0, 2));
+                   string currTime = "";
+                   if(ShowPostTime.Equals("few seconds ago")){
+                   sec = sec + 59;
+                   min += sec / 60;
+                   hrs += min / 60;
+                   sec = sec % 60;
+                   min = min % 60;
+                   hrs = hrs % 24;
+                   }
+                   else if(ShowPostTime.Contains(" minutes ago")){
+                       int ind = ShowPostTime.IndexOf(' '); // 59 mi
+                       int X = Convert.ToInt32(ShowPostTime.Substring(0, ind));
+                       min += X ;
+                       hrs += min / 60;
+                       min = min % 60;
+                       hrs = hrs % 24;
+                    }
+                    else if(ShowPostTime.Contains(" hours ago")){
+                        int ind = ShowPostTime.IndexOf(' ');
+                        int X = Convert.ToInt32(ShowPostTime.Substring(0, ind));
+                        hrs = hrs + 23;
+                        hrs = hrs % 24;
+                    }
+                    //handled all three cases above
+        string strHrs = "";
+        if (hrs == 0) strHrs = hrs.ToString() + hrs.ToString();
+        else strHrs = hrs.ToString();
+         currTime = strHrs+":"+min.ToString()+":"+sec.ToString();
+        if(ShowPostTime.Equals("few seconds ago")){
+         if(ExactPostTime.CompareTo(currTime) < 0) return ExactPostTime;
+         else return currTime;
+        }
+          return currTime;
+        }
+
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            string currminTime = "00:00:00";
+            //should contain same number of elements
+            if(exactPostTime.Length != showPostTime.Length) return "impossible";
+            if(exactPostTime.Length == 1){
+                return process(exactPostTime[0], showPostTime[0]);
+            }
+            
+            else{
+            for(int i = 0; i < exactPostTime.Length - 1; i++){
+                //cannot have two different human readable messages for same time
+                if(exactPostTime[i].Equals(exactPostTime[i+1]) && !(showPostTime[i].Equals(showPostTime[i+1])))
+                    return "impossible";
+            }
+            
+            string temp = "";
+            for(int i = 0; i < exactPostTime.Length; i++){
+                temp = process(exactPostTime[i],showPostTime[i]);
+                if(currminTime.CompareTo(temp) < 0) currminTime = temp;
+            }
+            }
+
+            return currminTime;
+            //throw new NotImplementedException();
         }
     }
 }
