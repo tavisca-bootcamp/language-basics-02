ef60635130940c520a38c7e44d6e161408518ad2
tavisca-dshrivastav <dshrivastav@tavisca.in>
2019-06-21 10:58:52 +0530

    Program code implemented

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -23,7 +23,41 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            // Add your code here.
+            var currTime = new List<TimeSpan>();
+            for (int i=0;i<exactPostTime.Length;i++)
+            {
+                for(int j=i+1;j<exactPostTime.Length;j++)
+                {
+                    if(exactPostTime[i]==exactPostTime[j])
+                    {
+                        if (showPostTime[i] != showPostTime[j])
+                            return "impossible";
+                    }
+                }
+                TimeSpan timeSpan = TimeSpan.Parse(exactPostTime[i]);
+                if (showPostTime[i].Contains("few"))
+                {
+                    TimeSpan time = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    currTime.Add(time);
+                }
+                else if(showPostTime[i].Contains("minutes"))
+                {
+                    string minute = showPostTime[i].Split(' ')[0];
+                    timeSpan = timeSpan.Add(TimeSpan.FromMinutes(double.Parse(minute)));
+                    TimeSpan time = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    currTime.Add(time);
+                }
+                else
+                {
+                    string hours = showPostTime[i].Split(' ')[0];
+                    timeSpan = timeSpan.Add(TimeSpan.FromHours(double.Parse(hours)));
+                    TimeSpan time = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    currTime.Add(time);
+                }
+                
+            }
+            currTime.Sort();
+            return currTime[currTime.Count - 1].ToString();
             throw new NotImplementedException();
         }
     }
