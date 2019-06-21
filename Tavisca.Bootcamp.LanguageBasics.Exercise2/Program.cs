f1468b3b34494f044cc9c25cac7b46e8ac9c9d21
tavisca-vraut <vraut@tavisca.in>
2019-06-21 10:11:44 +0530

    completed function

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -23,8 +23,197 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int[] previousStart = null;
+            int[] previousEnd = null;
+
+            for (int i = 0; i < exactPostTime.Length; i++) {
+                DateTime prevDateStart = DateTime.Now,
+                         prevDateEnd = DateTime.Now,
+                        currDateStart = DateTime.Now, 
+                        currDateEnd = DateTime.Now;
+                // Find time intersection of current and previous
+                if (previousStart == null && previousEnd == null) {
+                    string[] currentStartSplit = exactPostTime[i].Split(':');
+                    previousStart = new[] {int.Parse(currentStartSplit[0]), 
+                                           int.Parse(currentStartSplit[1]),
+                                           int.Parse(currentStartSplit[2])};
+
+                    int[] getTime_ = Program.getTime(showPostTime[i]);
+                    
+                    if (getTime_[3] == 1) {
+                        previousStart[0] = (getTime_[0] + previousStart[0]) % 24;
+                        getTime_[0] = previousStart[0];
+                        getTime_[1] = previousStart[1];
+                        getTime_[2] = previousStart[2];
+
+                        getTime_[2] = (getTime_[2] + 59);
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        getTime_[1] = (getTime_[1] + 59);
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                        
+                        
+                    } else if (getTime_[4] == 1) {
+
+                        previousStart[1] = (getTime_[1] + previousStart[1]);
+                        if (previousStart[1] >= 60) previousStart[0] = (previousStart[0] + 1) % 24;
+                        previousStart[1] %= 60;
+                        getTime_[1] = previousStart[1];
+                        getTime_[0] = previousStart[0];
+                        getTime_[2] = previousStart[2];
+                        
+                        getTime_[2] = (getTime_[2] + 59);
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                        
+                    } else {
+                        getTime_[2] = (getTime_[2] + previousStart[2]);
+                        getTime_[1] = previousStart[1];
+                        getTime_[0] = previousStart[0];
+
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                    }
+                    previousEnd = new[] {getTime_[0], getTime_[1], getTime_[2]};
+                    continue;
+                }
+                // else, update the previous to be intersection
+                else {
+                    string[] currentStartSplit = exactPostTime[i].Split(':');
+                    int[] currentStart = new[] {int.Parse(currentStartSplit[0]),
+                                                int.Parse(currentStartSplit[1]),
+                                                int.Parse(currentStartSplit[2])};
+
+                    int[] getTime_ = Program.getTime(showPostTime[i]);
+                    
+                    if (getTime_[3] == 1) {
+                        currentStart[0] = (getTime_[0] + currentStart[0]) % 24;
+                        getTime_[0] = currentStart[0];
+                        getTime_[1] = currentStart[1];
+                        getTime_[2] = currentStart[2];
+
+                        getTime_[2] = (getTime_[2] + 59);
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        getTime_[1] = (getTime_[1] + 59);
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                        
+                    } else if (getTime_[4] == 1) {
+
+                        currentStart[1] = (getTime_[1] + currentStart[1]);
+                        if (currentStart[1] >= 60) currentStart[0] = (currentStart[0] + 1) % 24;
+                        currentStart[1] %= 60;
+                        getTime_[1] = currentStart[1];
+                        getTime_[0] = currentStart[0];
+                        getTime_[2] = currentStart[2];
+                        
+                        getTime_[2] = (getTime_[2] + 59);
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                    } else {
+                        getTime_[2] = (getTime_[2] + currentStart[2]);
+                        getTime_[1] = currentStart[1];
+                        getTime_[0] = currentStart[0];
+
+                        if (getTime_[2] >= 60) getTime_[1] += 1;
+                        getTime_[2] %= 60;
+
+                        if (getTime_[1] >= 60) getTime_[0] += 1;
+                        getTime_[1] %= 60;
+                        getTime_[0] %= 24;
+                    }
+                    int[] currentEnd = new[] {getTime_[0], getTime_[1], getTime_[2]};
+
+                    prevDateStart = new DateTime(1, 1, 1, previousStart[0], previousStart[1], previousStart[2]);
+                    currDateStart = new DateTime(1, 1, 1, currentStart[0], currentStart[1], currentStart[2]);
+
+                    if (previousStart[0] <= previousEnd[0]) {
+                        prevDateEnd = new DateTime(1, 1, 1, previousEnd[0], previousEnd[1], previousEnd[2]);
+                    } else {
+                        prevDateEnd = new DateTime(1, 1, 2, previousEnd[0], previousEnd[1], previousEnd[2]);
+                    }
+
+                    if (currentStart[0] <= currentEnd[0]) {
+                        currDateEnd = new DateTime(1, 1, 1, currentEnd[0], currentEnd[1], currentEnd[2]);
+                    } else {
+                        currDateEnd = new DateTime(1, 1, 2, currentEnd[0], currentEnd[1], currentEnd[2]);
+                    }
+
+                    if (DateTime.Compare(currDateStart, prevDateStart) >= 0 &&
+                        DateTime.Compare(currDateStart, prevDateEnd) <= 0 &&
+                        DateTime.Compare(currDateEnd, prevDateEnd) >=0) {
+                            previousStart = new[] {currDateStart.Hour, currDateStart.Minute, currDateStart.Second};
+                            previousEnd = new[] {prevDateEnd.Hour, prevDateEnd.Minute, prevDateEnd.Second};
+                    } else if (DateTime.Compare(currDateStart, prevDateStart) <= 0 &&
+                                DateTime.Compare(prevDateEnd, currDateEnd) <=0 ) {
+                            previousStart = new[] {prevDateStart.Hour, prevDateStart.Minute, prevDateStart.Second};
+                            previousEnd = new[] {prevDateEnd.Hour, prevDateEnd.Minute, prevDateEnd.Second};
+                    } else if (DateTime.Compare(prevDateStart, currDateStart) >= 0 &&
+                        DateTime.Compare(prevDateStart, currDateEnd) <= 0 &&
+                        DateTime.Compare(prevDateEnd, currDateEnd) >= 0) {
+                            previousStart = new[] {prevDateStart.Hour, prevDateStart.Minute, prevDateStart.Second};
+                            previousEnd = new[] {currDateEnd.Hour, currDateEnd.Minute, currDateEnd.Second};
+                    } else if (DateTime.Compare(prevDateStart, currDateStart) <= 0 &&
+                                DateTime.Compare(currDateEnd, prevDateEnd) <= 0) {
+                            previousStart = new[] {currDateStart.Hour, currDateStart.Minute, currDateStart.Second};
+                            previousEnd = new[] {currDateEnd.Hour, currDateEnd.Minute, currDateEnd.Second};
+                    } else {
+                        return "impossible";
+                    }
+                }
+            }
+
+            // return lexicographically smallest string
+            if (previousEnd[0] < previousStart[0]) {
+                return "00:00:00";
+            } 
+            string hr = $"{previousStart[0]}";
+            if (hr.Length == 1) hr = "0" + hr;
+            string min = $"{previousStart[1]}";
+            if (min.Length == 1) min = "0" + min;
+            string sec = $"{previousStart[2]}";
+            if (sec.Length == 1) sec = "0" + sec;
+            return $"{hr}:{min}:{sec}";
+        }
+
+        public static int[] getTime(string post) {
+            var time_ = post.Split(' ');
+
+            int duration;
+            if (time_[0].Length == 3) {
+                duration = 59;
+            } else {
+                duration = int.Parse(time_[0]);
+            }
+
+            // 3rd index: posted hours ago?
+            // 4rd index: posted minutes ago?
+            // 5rd index: posted seconds ago?
+            if (post.IndexOf("seconds") != -1) {
+                return new[] {0, 0, duration, 0, 0, 1};
+            } else if (post.IndexOf("minutes") != -1) {
+                return new[] {0, duration, 0, 0, 1, 0};
+            } else {
+                return new[] {duration, 0, 0, 1, 0, 0};
+            }
         }
     }
 }
