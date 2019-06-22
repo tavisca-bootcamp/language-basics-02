d494c1fe434fea696415909b6167ad77b0d74127
Md Ebran Khan <ekhan@tavisca.in>
2019-06-22 19:32:32 +0530

    Preparing for assignment 2.2 to submit

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,30 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
-    public static class Program
+  public static class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
-            Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
-            Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
-            Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
-            Console.ReadKey(true);
-        }
-
-        private static void Test(string[] postTimes, string[] showTimes, string expected)
-        {
-            var result = GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
-            var postTimesCsv = string.Join(", ", postTimes);
-            var showTimesCsv = string.Join(", ", showTimes);
-            Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
-        }
-
-        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
-        {
-            // Add your code here.
-            throw new NotImplementedException();
-        }
+      Test(new[] { "12:12:12" }, new[] { "few seconds ago" }, "12:12:12");
+      Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
+      Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
+      Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
+      Console.ReadKey(true);
     }
+
+    private static void Test(string[] postTimes, string[] showTimes, string expected)
+    {
+      var result = GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
+      var postTimesCsv = string.Join(", ", postTimes);
+      var showTimesCsv = string.Join(", ", showTimes);
+      Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
+    }
+
+    public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+    {
+      var ct = new List<TimeSpan>();
+      int l = exactPostTime.Length;
+      for (int i = 0; i < l; i++)
+      {
+        for (int j = i + 1; j < l; j++)
+        {
+          if (exactPostTime[i] == exactPostTime[j])
+            if (showPostTime[i] != showPostTime[j])
+              return "impossible";
+        }
+
+        TimeSpan ts = TimeSpan.Parse(exactPostTime[i]);
+        if (showPostTime[i].Contains("few"))
+        {
+          TimeSpan time = new TimeSpan(ts.Hours, ts.Minutes, ts.Seconds);
+          ct.Add(time);
+        }
+        else if (showPostTime[i].Contains("minutes"))
+        {
+          string minute = showPostTime[i].Split(' ')[0];
+          ts = ts.Add(TimeSpan.FromMinutes(double.Parse(minute)));
+          TimeSpan time = new TimeSpan(ts.Hours, ts.Minutes, ts.Seconds);
+          ct.Add(time);
+        }
+        else
+        {
+          string hours = showPostTime[i].Split(' ')[0];
+          ts = ts.Add(TimeSpan.FromHours(double.Parse(hours)));
+          TimeSpan time = new TimeSpan(ts.Hours, ts.Minutes, ts.Seconds);
+          ct.Add(time);
+        }
+
+      }
+      ct.Sort();
+      return ct[ct.Count - 1].ToString();
+      throw new NotImplementedException();
+    }
+  }
 }
