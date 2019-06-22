6b0709fae186d4c6c668c6459f81f10622434bb6
tavisca-akhatri <akhatri@tavisca.in>
2019-06-22 22:38:58 +0530

    assignment 2 completed

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -24,7 +24,59 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            
+             TimeSpan max = TimeSpan.Parse("0");
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                for (int j = i + 1; j < exactPostTime.Length; j++)
+                {
+                    if (exactPostTime[i] == exactPostTime[j])
+                        if (showPostTime[i] != showPostTime[j])
+                            return "impossible";
+                }
+            }
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                TimeSpan time = TimeSpan.Parse(exactPostTime[i]);
+                if (showPostTime[i].Contains("sec"))
+                {
+                    if (max < time)
+                        max = time;
+
+                }
+                else
+                {
+                    int val = int.Parse(showPostTime[i].Substring(0, showPostTime[i].IndexOf(" ")));
+                    if (showPostTime[i].Contains("min"))
+                    {
+                        TimeSpan span = TimeSpan.FromMinutes(val);
+                        if (time + span > TimeSpan.Parse("1.00:00:00"))
+                        {
+                            time = time + span - TimeSpan.FromDays(1);
+                            if (max < time)
+                                max = time;
+                        }
+                        else
+                             if (max < time + span)
+                            max = time + span;
+                    }
+                    if (showPostTime[i].Contains("hour"))
+                    {
+                        TimeSpan span = TimeSpan.FromHours(val);
+                        if (time + span > TimeSpan.Parse("1.00:00:00"))
+                        {
+                            time = time + span - TimeSpan.FromDays(1);
+                            if (max < time)
+                                max = time;
+                        }
+                        else
+                            if (max < time + span)
+                            max = time + span;
+
+                    }
+                }
+            }
+            return max.ToString();
         }
     }
 }
