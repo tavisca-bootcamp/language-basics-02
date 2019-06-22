460a4892b2749774c69a5329c56e04448dfb967f
tavisca-arupade <51902733+tavisca-arupade@users.noreply.github.com>
2019-06-22 20:47:27 +0530

    Completed GetCurrentTime function

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -23,8 +23,70 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            // Author : Aditi Rupade.
+
+            TimeSpan currentTime = new TimeSpan(0),  // to store current times for each iteration
+                 posttime  = new TimeSpan(0),     // to store parsed post times
+                 latervalue = new TimeSpan(0);    // to store time after adding the message times
+
+            // for same post times but showing different messages
+            if (exactPostTimes.Distinct().Count() != showPostTimes.Distinct().Count())
+                return "impossible";
+
+            // iterating for each value in exactPostTimes array
+            for (int i = 0; i < exactPostTimes.Length; i++)
+            {
+                //parsing and splitting 
+                posttime = TimeSpan.Parse(exactPostTimes[i]);
+                string[] val = showPostTimes[i].Split(' ');
+
+
+                //Case 1: Checking for seconds
+                if (val[1] =="seconds")
+                {
+                    if ( posttime > currentTime)
+                        currentTime = posttime;
+
+                }
+                else
+                {
+                    int value = int.Parse(val[0]);
+
+                    //Case 2: Checking for minutes
+                    if (val[1].Contains("min"))
+                    {
+                        latervalue = TimeSpan.FromMinutes(value);
+
+                        currentTime = TimeSpan.Parse(GetCurrentValue(latervalue, posttime, currentTime));
+                    }
+
+                    //Case 3: checking for hours
+                    if (val[i].Contains("hour"))
+                    {
+                        latervalue = TimeSpan.FromHours(value);
+
+                        currentTime = TimeSpan.Parse(GetCurrentValue(latervalue, posttime, currentTime));
+                    }
+                }
+            }
+            return currentTime.ToString();
+        }
+        
+        public static string GetCurrentValue(TimeSpan latervalue, TimeSpan posttime, TimeSpan currentTime)
+        {
+            //if current time is next day
+            if (posttime + latervalue > TimeSpan.Parse("1.00:00:00"))
+            {
+                posttime = posttime + latervalue - TimeSpan.FromDays(1);
+                if (posttime > currentTime)
+                    currentTime = posttime;
+            }
+            //if current time is same day
+            else
+                 if (posttime + latervalue > currentTime)
+                currentTime = posttime + latervalue;
+
+            return currentTime.ToString();
         }
     }
 }
