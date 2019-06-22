4acac4ece3a9576cbde169e9145d9846d974a366
SHIVAM RAJ <51924685+tavisca-sraj@users.noreply.github.com>
2019-06-22 16:05:57 +0530

    Exercise 1 - Assignment 2 Solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -20,10 +21,48 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var showTimesCsv = string.Join(", ", showTimes);
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
-
-        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+        public static string GetCurrentTime(string[] exactTime, string[] showTime)
         {
-            // Add your code here.
+            var cTime = new List<TimeSpan>();
+            for (int i=0;i<exactTime.Length;i++)
+            {
+				//Impossible Case
+                for(int j=i+1;j<exactTime.Length;j++)
+                {
+                    if(exactTime[i]==exactTime[j])
+                    {
+                        if (showTime[i] != showTime[j])
+                            return "impossible";
+                    }
+                }
+                TimeSpan timeSpan = TimeSpan.Parse(exactTime[i]);
+                //Case 1
+				if (showTime[i].Contains("few"))
+                {
+                    TimeSpan t = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    cTime.Add(t);
+                }
+				//Case 2
+                else if(showTime[i].Contains("minutes"))
+                {
+                    string minute = showTime[i].Split(' ')[0];
+                    timeSpan = timeSpan.Add(TimeSpan.FromMinutes(double.Parse(minute)));
+                    TimeSpan t = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    cTime.Add(t);
+                }
+				//Case 3 
+                else
+                {
+                    string hours = showTime[i].Split(' ')[0];
+                    timeSpan = timeSpan.Add(TimeSpan.FromHours(double.Parse(hours)));
+                    TimeSpan t = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    cTime.Add(t);
+                }
+
+            }
+            cTime.Sort();
+			int minIndex=cTime.Count - 1;
+            return cTime[minIndex].ToString();
             throw new NotImplementedException();
         }
     }
