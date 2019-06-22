1e28a0b01f1c0bed123dfa69b5ea9181e0ea3308
tavisca-dgupta <51922254+tavisca-dgupta@users.noreply.github.com>
2019-06-22 21:00:27 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -21,10 +23,85 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
-        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+       public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            if(!(isSame(exactPostTime).Equals(isSame(showPostTime))))//will check if exactTime is same then PostTime should be same
+            {
+                return "impossible";
+            }
+            string postTime="";
+            string[] time=Regex.Split(exactPostTime[0],":");//split the original time to obtain hr min and sec
+            for(int i=0;i<exactPostTime.Count();i++)
+            {
+                time=Regex.Split(exactPostTime[i],":");//split the original time to obtain hr min and sec
+                if(showPostTime[i].Contains("seconds"))
+                {
+                   postTime=exactPostTime[i];//if few sec ago the lexicographically smaller will be the time itself
+                }
+                else if(showPostTime[i].Contains("minutes"))
+                {
+                    String[] shownTime=Regex.Split(showPostTime[i]," ");//spliting the shown sentence to obtain mins
+                    if(((int.Parse(time[1])+int.Parse(shownTime[0]))%60)!=0)
+                    {
+                        time[0]=(((int.Parse(time[1]))+1)%24).ToString();//if by adding mins are converted to hours i.e min>60 the increase in hrs
+                        time[1]=((int.Parse(time[1])+int.Parse(shownTime[0]))%60).ToString();//add remaing mins to string
+                    }
+                    else
+                    {
+                        time[1]=((int.Parse(time[1])+int.Parse(shownTime[0]))%60).ToString();
+                    }
+                    postTime=timeFormat(time[0],time[1],time[2]);//timeformat convert time in proper format
+                        
+                }
+                else if(showPostTime[i].Contains("hours"))
+                {
+                     String[] shownTime=Regex.Split(showPostTime[i]," ");// to obtain hrs ago               
+                    if(String.IsNullOrWhiteSpace(postTime)!=true)//get the latest time so that to find lexicographically smaller time
+                    {
+                        String[] timeNow=Regex.Split(postTime,":");
+                        time[0]=(((int.Parse(time[0])+int.Parse(shownTime[0]))%24)).ToString();
+                        postTime=timeFormat(time[0],timeNow[1],timeNow[2]);
+                    }
+                    else
+                    {
+                        time[0]=(((int.Parse(time[0])+int.Parse(shownTime[0]))%24)).ToString();
+                        postTime=timeFormat(time[0],time[1],time[2]);
+                    }
+                }
+        }
+            return postTime;
+        
+        }
+        public static string timeFormat(string hr,string min,string sec)
+        {
+            if(hr.Length==1)
+            {
+                hr="0"+hr;
+            }
+            else if(min.Length==1)
+            {
+                min="0"+min;
+            }
+            else if(sec.Length==1)
+            {
+                sec="0"+sec;
+            }
+            return hr+":"+min+":"+sec;
+        }
+        public static bool isSame(string[] timeArray)
+        {
+            for(int i=0;i<timeArray.Count();i++)
+            {
+                for(int j=1;j<timeArray.Count();j++)
+                {
+                    if(timeArray[i]!=timeArray[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
