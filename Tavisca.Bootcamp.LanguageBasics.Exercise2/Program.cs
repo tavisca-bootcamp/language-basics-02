e1451e644d1752a5a0e3b176c4fe5e8bc3760ed4
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-06-22 06:56:17 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -6,7 +6,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
     {
         static void Main(string[] args)
         {
-            Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
+            Test(new[] { "12:12:12" }, new[] { "few seconds ago" }, "12:12:12");
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
             Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
             Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
@@ -24,7 +24,155 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            string min = "00:00:00", max = "23:59:59";
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                string prev = "";
+                string next = "";
+                int hours, minutes, seconds;
+                hours = Convert.ToInt32(exactPostTime[i].Substring(0, 2));
+                minutes = Convert.ToInt32(exactPostTime[i].Substring(3, 2));
+                seconds = Convert.ToInt32(exactPostTime[i].Substring(6, 2));
+                string[] humanMsg = showPostTime[i].Split(" ");
+                if (string.Compare(humanMsg[1], "seconds") == 0)
+                {
+                    prev = exactPostTime[i];
+                    seconds = seconds + 59;
+                    if (seconds % 60 != seconds)
+                    {
+                        seconds = seconds % 60;
+                        minutes++;
+                        if (minutes % 60 != minutes)
+                        {
+                            minutes = minutes % 60;
+                            hours++;
+                            if (hours % 24 != hours)
+                            {
+                                hours = hours % 24;
+
+                            }
+                        }
+                    }
+                    string h = hours.ToString(), m = minutes.ToString(), s = seconds.ToString();
+                    if (hours <= 9)
+                        h = "0" + hours.ToString();
+                    if (minutes <= 9)
+                        m = ":0" + minutes.ToString();
+                    if (seconds <= 9)
+                        s = ":0" + seconds.ToString();
+                    next = h + ":" + m + ":" + s;
+
+
+                }
+                else if (string.Compare(humanMsg[1], "minutes") == 0)
+                {
+                    minutes = minutes + Int32.Parse(humanMsg[0]);
+                    if (minutes % 60 != minutes)
+                    {
+                        minutes = minutes % 60;
+                        hours++;
+                        if (hours % 24 != hours)
+                        {
+                            hours = hours % 24;
+
+                        }
+                    }
+                    string h = hours.ToString(), m = minutes.ToString(), s = seconds.ToString();
+                    if (hours <= 9)
+                        h = "0" + hours.ToString();
+                    if (minutes <= 9)
+                        m = ":0" + minutes.ToString();
+                    if (seconds <= 9)
+                        s = ":0" + seconds.ToString();
+                    prev = h + ":" + m + ":" + s;
+
+                    seconds = seconds + 59;
+                    if (seconds % 60 != seconds)
+                    {
+                        seconds = seconds % 60;
+                        minutes++;
+                        if (minutes % 60 != minutes)
+                        {
+                            minutes = minutes % 60;
+                            hours++;
+                            if (hours % 24 != hours)
+                            {
+                                hours = hours % 24;
+
+                            }
+                        }
+
+
+                    }
+                    h = hours.ToString(); m = minutes.ToString(); s = seconds.ToString();
+                    if (hours <= 9)
+                        h = "0" + hours.ToString();
+                    if (minutes <= 9)
+                        m = ":0" + minutes.ToString();
+                    if (seconds <= 9)
+                        s = ":0" + seconds.ToString();
+                    next = h + ":" + m + ":" + s;
+                }
+                else
+                {
+                    hours = hours + Int32.Parse(humanMsg[0]);
+                    hours = hours % 24;
+                    string h = hours.ToString(), m = minutes.ToString(), s = seconds.ToString();
+                    if (hours <= 9)
+                        h = "0" + hours.ToString();
+                    if (minutes <= 9)
+                        m = ":0" + minutes.ToString();
+                    if (seconds <= 9)
+                        s = ":0" + seconds.ToString();
+                    prev = h + ":" + m + ":" + s;
+                    seconds = seconds + 59;
+                    if (seconds % 60 != seconds)
+                    {
+                        seconds = seconds % 60;
+                        minutes++;
+                        if (minutes % 60 != minutes)
+                        {
+                            minutes = minutes % 60;
+                            hours++;
+                            if (hours % 24 != hours)
+                            {
+                                hours = hours % 24;
+
+                            }
+                        }
+
+
+                    }
+                    minutes = minutes + 59;
+                    if (minutes % 60 != minutes)
+                    {
+                        minutes = minutes % 60;
+                        hours++;
+                        if (hours % 24 != hours)
+                        {
+                            hours = hours % 24;
+
+                        }
+                    }
+                    h = hours.ToString(); m = minutes.ToString(); s = seconds.ToString();
+                    if (hours <= 9)
+                        h = "0" + hours.ToString();
+                    if (minutes <= 9)
+                        m = ":0" + minutes.ToString();
+                    if (seconds <= 9)
+                        s = ":0" + seconds.ToString();
+                    next = h + ":" + m + ":" + s;
+
+                }
+                if (string.Compare(min, prev) == -1)
+                    min = prev;
+                if (string.Compare(max, next) == 1)
+                    max = next;
+            }
+            if (string.Compare(min, max) == -1)
+                return min;
+            return "impossible";
         }
     }
 }
