9ffec37d020e73f6439390e57c0727db5ca12d39
Rahul Tiwari <rtiwari@tavisca.in>
2019-06-22 23:27:04 +0530

    assignement 2

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -21,10 +21,51 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
-        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+        public static string GetCurrentTime(string[] exactPostTimes, string[] showPostTimes)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            string [] show = new string[3];
+            int len = exactPostTimes.Length;
+            DateTime [] output=new DateTime[2*len];
+            DateTime[] t = new DateTime[len];
+            DateTime[] t1 = new DateTime[2];
+            int k=0;
+            for(int i=0;i<len;i++){
+                for(int j=1;j<len;j++){
+                    if(exactPostTimes[i]==exactPostTimes[j]){
+                        if(showPostTimes[i]!=showPostTimes[j]){
+                            return "impossible";
+                        }
+                    }
+                }
+            }
+            for(int i=0 ; i<len;i++){
+                t[i] = DateTime.Parse(exactPostTimes[i]);
+                show= showPostTimes[i].Split(' ');
+                if(String.Equals(show[1],"seconds")){
+                output[2*i]=t[i];
+                output[2*i+1]=t[i].Add(new TimeSpan(0,0,59));
+                }
+            else if(String.Equals(show[1],"minutes")){
+                k = Int32.Parse(show[0]);
+                output[2*i]=t[i].Add(new TimeSpan(0,k,0));
+                output[2*i+1]=t[i].Add(new TimeSpan(0,k,59));
+                }
+            else{
+                    k = Int32.Parse(show[0]);
+                    output[2*i]=t[i].Add(new TimeSpan(k,0,0));
+                    output[2*i+1]=t[i].Add(new TimeSpan(k,59,59));
+                }
+            }
+            t1[0] = output[0];
+            t1[1] = output[1];
+            for(int i=1;i<len;i++){
+                if(output[2*i].TimeOfDay >t1[1].TimeOfDay || t1[0].TimeOfDay> output[2*i+1].TimeOfDay){
+                    return "impossible";
+                }
+                t1[0]= t1[0].TimeOfDay>output[2*i].TimeOfDay ? t1[0] : output[2*i];
+                t1[1]= t1[1].TimeOfDay<output[2*i+1].TimeOfDay ? t1[1] : output[2*i+1];
+            }
+            return t1[0].TimeOfDay.ToString();
         }
     }
 }
