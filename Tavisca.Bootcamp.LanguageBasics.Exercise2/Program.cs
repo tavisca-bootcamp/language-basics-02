dcf3195a10e7cccde5009d5630bc9f5d10656ad9
Tavisca-vvinod <51902376+Tavisca-vvinod@users.noreply.github.com>
2019-06-25 10:30:24 +0530

    Used DateTime and revised the code.

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -12,73 +12,6 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
             Console.ReadKey(true);
         }
-        public static string[] findstarts(string[] exactPostTime, string[] showPostTime)
-        {
-            string[] starts = new string[exactPostTime.Length];
-            int h, m, s;
-            for (int i=0;i<exactPostTime.Length;i++)
-            {
-                if(showPostTime[i].Contains("few"))
-                {
-                    starts[i] = exactPostTime[i];
-                }
-                else if (showPostTime[i].Contains("minutes"))
-                {
-                    String[] extramin = showPostTime[i].Split(' ');
-                    String[] hms = exactPostTime[i].Split(':');
-                    h = Int32.Parse(hms[0]);
-                    m = Int32.Parse(hms[1]);
-                    s = Int32.Parse(hms[2]);
-                    starts[i] = addtime(h, m, s, 0, Int32.Parse(extramin[0]), 0);
-                }
-                if (showPostTime[i].Contains("hours"))
-                {
-                    String[] extrahours = showPostTime[i].Split(' ');
-                    String[] hms = exactPostTime[i].Split(':');
-                    h = Int32.Parse(hms[0]);
-                    m = Int32.Parse(hms[1]);
-                    s = Int32.Parse(hms[2]);
-                    starts[i] = addtime(h, m, s, Int32.Parse(extrahours[0]), 0, 0);
-                }
-            }
-            return starts;
-        }
-        public static string[] findends(string[] exactPostTime, string[] showPostTime)
-        {
-            string[] ends = new string[exactPostTime.Length];
-            int h, m, s;
-            for (int i = 0; i < exactPostTime.Length; i++)
-            {
-                if (showPostTime[i].Contains("few"))
-                {
-                    String[] hms = exactPostTime[i].Split(':');
-                    h = Int32.Parse(hms[0]);
-                    m = Int32.Parse(hms[1]);
-                    s = Int32.Parse(hms[2]);
-                    ends[i] = addtime(h, m, s, 0, 0, 59);
-                    
-                }
-                else if (showPostTime[i].Contains("minutes"))
-                {
-                    String[] extramin = showPostTime[i].Split(' ');
-                    String[] hms = exactPostTime[i].Split(':');
-                    h = Int32.Parse(hms[0]);
-                    m = Int32.Parse(hms[1]);
-                    s = Int32.Parse(hms[2]);
-                    ends[i] = addtime(h, m, s, 0, Int32.Parse(extramin[0]), 59);
-                }
-                if (showPostTime[i].Contains("hours"))
-                {
-                    String[] extrahours = showPostTime[i].Split(' ');
-                    String[] hms = exactPostTime[i].Split(':');
-                    h = Int32.Parse(hms[0]);
-                    m = Int32.Parse(hms[1]);
-                    s = Int32.Parse(hms[2]);
-                    ends[i] = addtime(h, m, s, Int32.Parse(extrahours[0]), 59, 59);
-                }
-            }
-            return ends;
-        }
         private static void Test(string[] postTimes, string[] showTimes, string expected)
         {
             var result = GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
@@ -86,166 +19,47 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var showTimesCsv = string.Join(", ", showTimes);
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
-
-        public static string singleindex(String exactPostTime, string showPostTime)
-        {
-            if (showPostTime.Contains("few"))
-            {
-                return exactPostTime;
-            }
-            if (showPostTime.Contains("minutes"))
-            {
-                String[] extramin = showPostTime.Split(' ');
-                String[] hms = exactPostTime.Split(':');
-                int h = Int32.Parse(hms[0]);
-                int m = Int32.Parse(hms[1]);
-                int s = Int32.Parse(hms[2]);
-                string answer = addtime(h,m,s,0,Int32.Parse(extramin[0]),0);
-                return answer;
-            }
-            if (showPostTime.Contains("hours"))
-            {
-                String[] extrahours = showPostTime.Split(' ');
-                String[] hms = exactPostTime.Split(':');
-                int h = Int32.Parse(hms[0]);
-                int m = Int32.Parse(hms[1]);
-                int s = Int32.Parse(hms[2]);
-                string answer = addtime(h, m, s, Int32.Parse(extrahours[0]),0, 0);
-                return answer;
-            }
-            return null;
-        }
-        public static int checkin(string s,string t,string e)
-        {
-            String[] sms = s.Split(':');
-            int sh = Int32.Parse(sms[0]);
-            int sm = Int32.Parse(sms[1]);
-            int ss = Int32.Parse(sms[2]);
-            String[] tms = t.Split(':');
-            int th = Int32.Parse(tms[0]);
-            int tm = Int32.Parse(tms[1]);
-            int ts = Int32.Parse(tms[2]);
-            String[] ems =e.Split(':');
-            int eh = Int32.Parse(ems[0]);
-            int em = Int32.Parse(ems[1]);
-            int es = Int32.Parse(ems[2]);
-            if (em == 0)
-            {
-                em = 60;
-            }
-            if(sh<=th && th<=eh)
-            {
-                if((sm<=tm && tm<=em)||(th<eh && tm>=sm))
-                {
-                    if ((ss<=ts && ts<=es)||(tm<em && ss<=ts)||(th<eh&&tm>sm))
-                    {
-                        return 1;
-                    }
-                }
-            }
-            return 0;
-        }
-        public static string addtime(int h,int m,int s,int eh,int em,int es)
-        {
-            int anh=0,anm=0,ans=0;
-            string a, b, c;
-            if((es+s)<60)
-            {
-                ans= es + s;
-            }
-            else 
-            {
-                ans = (es + s - 60);
-                em = em + 1;
-            }
-            if(em+m<60)
-            {
-                anm = em + m;
-            }
-            else
-            {
-                anm = em + m - 60;
-                eh = eh + 1;
-            }
-            anh = (eh + h)%24;
-            if (anh==24)
-            {
-                anh = 0;
-            }
-            if (anh < 10)
-            {
-                 a = "0"+anh.ToString();
-            }
-            else
-            {
-                 a =anh.ToString();
-            }
-            if (anm < 10)
-            {
-                 b = "0" + anm.ToString();
-            }
-            else
-            {
-                 b = anm.ToString();
-            }
-            if (ans < 10)
-            {
-                 c = "0" + ans.ToString();
-            }
-            else
-            {
-                 c = ans.ToString();
-            }
-            string answer = a+":"+ b +":"+ c;
-            return answer;
-
-        }
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            string answer = null;
-            if(exactPostTime.Length==1)
+            for (int i = 0; i < exactPostTime.Length; i++)
             {
-                answer = singleindex(exactPostTime[0], showPostTime[0]);
-                return answer;
-            }
-            int count = 0;
-            for (int i=0;i<showPostTime.Length;i++)
-            {
-                for(int j=i+1;j<showPostTime.Length;j++)
+                for (int j = i + 1; j < exactPostTime.Length; j++)
                 {
-                    if(exactPostTime[i]==exactPostTime[j])
+                    if (exactPostTime[i] == exactPostTime[j] && showPostTime[i] != showPostTime[j])
                     {
-                        count++;
-                        if(showPostTime[i]!=showPostTime[j])
-                        {
                             return "impossible";
-                        }
                     }
                 }
             }
-            if((count*2)==exactPostTime.Length)
+            string[] CurrentArray = new string[exactPostTime.Length];
+            for (int i = 0; i < exactPostTime.Length; i++)
             {
-                answer = singleindex(exactPostTime[0], showPostTime[0]);
-                return answer;
-            }
-            string[] starts = findstarts(exactPostTime, showPostTime);
-            string[] ends = findends(exactPostTime, showPostTime);
-            for (int j=0;j<starts.Length;j++)
-            {
-                int checks = 0;
-                for (int i = 0; i<starts.Length; i++)
+                DateTime time = Convert.ToDateTime(exactPostTime[i]);               
+                if (showPostTime[i].Contains("seconds"))
                 {
-                    if (checkin(starts[i], starts[j], ends[i]) == 1)
-                    {
-                        checks++;
-                    }
+                    CurrentArray[i] = exactPostTime[i];
                 }
-                if(checks==starts.Length)
+                else if (showPostTime[i].Contains("minutes"))
                 {
-                    return starts[j];
+                    string Minutes = showPostTime[i].Split(' ')[0];
+                    DateTime Current = time.AddMinutes(Int32.Parse(Minutes)); 
+                    string Result =  Current.ToString().Split(' ')[1];
+                    CurrentArray[i] = Result;
+                }
+                else if (showPostTime[i].Contains("hours"))
+                {
+                    string Hours = showPostTime[i].Split(' ')[0];
+                    DateTime Current = time.AddHours(Int32.Parse(Hours)); 
+                    string Result = Current.ToString().Split(' ')[1];
+                    CurrentArray[i] = Result;
+                }
+                else
+                {
+                    return "impossible";
                 }
             }
-            return "impossible";  
+            Array.Sort(CurrentArray);//Ascending order
+            return CurrentArray[exactPostTime.Length - 1];
         }
     }
 }
