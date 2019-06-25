cca80b79c55975f562c8394de2f7fb4549c524bb
tavisca-nnarkhede <51942815+tavisca-nnarkhede@users.noreply.github.com>
2019-06-25 09:27:24 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -23,7 +23,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
         }
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            var Answer = new List<TimeSpan>();
+            var answer = new List<TimeSpan>();
             for(int i=0;i<exactPostTime.Length;i++)
             {
                 for(int j=i+1;j<exactPostTime.Length;j++)
@@ -34,29 +34,29 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
                             return "impossible";
                     }
                 }
-                TimeSpan timespan=TimeSpan.Parse(exactPostTime[i]);
+                TimeSpan timeSpan=TimeSpan.Parse(exactPostTime[i]);
                 if (showPostTime[i].Contains("few"))
                 {
-                    TimeSpan timespan1=new TimeSpan(timespan.Hours,timespan.Minutes,timespan.Seconds);
-                    Answer.Add(timespan1);
+                    TimeSpan timespan1=new TimeSpan(timeSpan.Hours,timeSpan.Minutes,timeSpan.Seconds);
+                    answer.Add(timespan1);
                 }
                 else if(showPostTime[i].Contains("minutes"))
                 {
-                    string Minutes =showPostTime[i].Split(' ')[0];
-                    timespan=timespan.Add(TimeSpan.FromMinutes(double.Parse(Minutes)));
-                    TimeSpan timespan1 = new TimeSpan(timespan.Hours, timespan.Minutes, timespan.Seconds);
-                    Answer.Add(timespan1);
+                    string minutes =showPostTime[i].Split(' ')[0];
+                    timeSpan=timeSpan.Add(TimeSpan.FromMinutes(double.Parse(minutes)));
+                    TimeSpan timeSpan1 = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    answer.Add(timeSpan1);
                 }
                 else
                 {
-                    string Hours=showPostTime[i].Split(' ')[0];
-                    timespan=timespan.Add(TimeSpan.FromHours(double.Parse(Hours)));
-                    TimeSpan timespan1 = new TimeSpan(timespan.Hours, timespan.Minutes, timespan.Seconds);
-                    Answer.Add(timespan1);
+                    string hours=showPostTime[i].Split(' ')[0];
+                    timeSpan=timeSpan.Add(TimeSpan.FromHours(double.Parse(hours)));
+                    TimeSpan timeSpan1 = new TimeSpan(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+                    answer.Add(timeSpan1);
                 }
             }
-            Answer.Sort();
-            return Answer[Answer.Count-1].ToString();
+            answer.Sort();
+            return answer[answer.Count-1].ToString();
             throw new NotImplementedException();
         }
     }
