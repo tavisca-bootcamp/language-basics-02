a7758d3db414ba2b4a5d4ce9d7569a22f3858f38
Ujjwal Bharti <ubharti@tavisca.in>
2019-06-26 07:39:01 +0530

    Solved using one loop

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/ForumPostEasy.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/ForumPostEasy.cs
@@ -8,16 +8,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             var currentTime = new List<TimeSpan>();
+            if(CheckDifferentRedundant(exactPostTime,showPostTime))
+                return "impossible";
             for (int i = 0; i < exactPostTime.Length; i++)
             {
-                for (int j = i + 1; j < exactPostTime.Length; j++)
-                {
-                    if (exactPostTime[i] == exactPostTime[j])
-                    {
-                        if (showPostTime[i] != showPostTime[j])
-                            return "impossible";
-                    }
-                }
                 TimeSpan timeSpan = TimeSpan.Parse(exactPostTime[i]);
                 TimeSpan time = GetTimeSpan(timeSpan, showPostTime[i]);
                 currentTime.Add(time);
@@ -25,6 +19,23 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
             currentTime.Sort();
             return currentTime[currentTime.Count - 1].ToString();
         }
+
+        private static bool CheckDifferentRedundant(string[] exactPostTime, string[] showPostTime)
+        {
+            var tempList = new List<string>();
+            for(int i=0;i<exactPostTime.Length;i++)
+            {
+                if(tempList.Contains(exactPostTime[i]))
+                {
+                    var index = tempList.IndexOf(exactPostTime[i]);
+                    if (showPostTime[i] != showPostTime[index])
+                        return true;
+                }
+                tempList.Add(exactPostTime[i]);
+            }
+            return false;
+        }
+
         public static TimeSpan GetTimeSpan(TimeSpan timeSpan,string showPostTime)
         {
             string time = showPostTime.Split(' ')[0];
