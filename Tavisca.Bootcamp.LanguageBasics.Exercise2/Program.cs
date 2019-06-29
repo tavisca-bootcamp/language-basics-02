fc35d76c3bec9e14d400d1ff2b99a292a01b113d
Tavisca-vvinod <51902376+Tavisca-vvinod@users.noreply.github.com>
2019-06-29 10:15:53 +0530

    Updated Program.
    @arpit-nagar, sir please check this one, I have tried to get a deeper understanding of the problem.

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
+using System;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
     {
         static void Main(string[] args)
         {
-            Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
+            Test(new[] { "12:12:12" }, new[] { "few seconds ago" }, "12:12:12");
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
             Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
             Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
@@ -21,45 +20,159 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         }
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
-            for (int i = 0; i < exactPostTime.Length; i++)
+            string CurrentTime = null;
+            if (exactPostTime.Length == 1)
             {
-                for (int j = i + 1; j < exactPostTime.Length; j++)
+                CurrentTime = singleIndex(exactPostTime[0], showPostTime[0]);
+                return CurrentTime;
+            }
+            int Matches = 0;
+            for (int i = 0; i < showPostTime.Length; i++)
+            {
+                for (int j = i + 1; j < showPostTime.Length; j++)
                 {
-                    if (exactPostTime[i] == exactPostTime[j] && showPostTime[i] != showPostTime[j])
+                    if (exactPostTime[i] == exactPostTime[j])
                     {
+                        Matches++;
+                        if (showPostTime[i] != showPostTime[j])
+                        {
                             return "impossible";
+                        }
                     }
                 }
             }
-            string[] CurrentArray = new string[exactPostTime.Length];
+            if ((Matches * 2) == exactPostTime.Length)
+            {
+                CurrentTime = singleIndex(exactPostTime[0], showPostTime[0]);
+                return CurrentTime;
+            }
+            string[] UpperBounds = findLowerBounds(exactPostTime, showPostTime);
+            string[] LowerBound = findUpperBounds(exactPostTime, showPostTime);
+            for (int j = 0; j < UpperBounds.Length; j++)
+            {
+                int IntervalsMatched = 0;
+                for (int i = 0; i < UpperBounds.Length; i++)
+                {
+                    if (CheckIntervals(UpperBounds[i], UpperBounds[j], LowerBound[i]) == 1)
+                    {
+                        IntervalsMatched++;
+                    }
+                }
+                if (IntervalsMatched == UpperBounds.Length)//If the given time is falling in all intervals
+                {
+                    return UpperBounds[j];
+                }
+            }
+            return "impossible";
+        }
+        //findLowerBounds will return a string array of time which serve as 
+        //lower bounds of the intervals created by the showpostime and exactposttime
+        public static string[] findLowerBounds(string[] exactPostTime, string[] showPostTime)
+        {
+            string[] LowerBounds = new string[exactPostTime.Length];
             for (int i = 0; i < exactPostTime.Length; i++)
             {
-                DateTime time = Convert.ToDateTime(exactPostTime[i]);               
-                if (showPostTime[i].Contains("seconds"))
+                if (showPostTime[i].Contains("few"))
                 {
-                    CurrentArray[i] = exactPostTime[i];
+                    LowerBounds[i] = exactPostTime[i];
                 }
                 else if (showPostTime[i].Contains("minutes"))
                 {
-                    string Minutes = showPostTime[i].Split(' ')[0];
-                    DateTime Current = time.AddMinutes(Int32.Parse(Minutes)); 
-                    string Result =  Current.ToString().Split(' ')[1];
-                    CurrentArray[i] = Result;
+                    String[] Minutes = showPostTime[i].Split(' ');
+                    DateTime CurrentTime = Convert.ToDateTime(exactPostTime[i]);
+                    CurrentTime = CurrentTime.AddMinutes(Int32.Parse(Minutes[0]));
+                    LowerBounds[i]=CurrentTime.ToString().Split(' ')[1];
                 }
-                else if (showPostTime[i].Contains("hours"))
+                if (showPostTime[i].Contains("hours"))
                 {
-                    string Hours = showPostTime[i].Split(' ')[0];
-                    DateTime Current = time.AddHours(Int32.Parse(Hours)); 
-                    string Result = Current.ToString().Split(' ')[1];
-                    CurrentArray[i] = Result;
-                }
-                else
-                {
-                    return "impossible";
+                    String[] Hours = showPostTime[i].Split(' ');
+                    DateTime CurrentTime = Convert.ToDateTime(exactPostTime[i]);
+                    CurrentTime = CurrentTime.AddHours(Int32.Parse(Hours[0]));
+                    LowerBounds[i] = CurrentTime.ToString().Split(' ')[1];
                 }
             }
-            Array.Sort(CurrentArray);//Ascending order
-            return CurrentArray[exactPostTime.Length - 1];
+            return LowerBounds;
+        }
+        //findUpperBounds will return a string array of time which serve as 
+        //upper bounds of the intervals created by the showpostime and exactposttime
+        public static string[] findUpperBounds(string[] exactPostTime, string[] showPostTime)
+        {
+            string[] UpperBounds = new string[exactPostTime.Length];
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                if (showPostTime[i].Contains("few"))
+                {
+                    DateTime CurrentTime = Convert.ToDateTime(exactPostTime[i]);
+                    CurrentTime = CurrentTime.AddSeconds(59);
+                    UpperBounds[i] = CurrentTime.ToString().Split(' ')[1];
+                }
+                else if (showPostTime[i].Contains("minutes"))
+                {
+                    String[] Minutes = showPostTime[i].Split(' ');
+                    DateTime CurrentTime = Convert.ToDateTime(exactPostTime[i]);
+                    CurrentTime = CurrentTime.AddMinutes(Int32.Parse(Minutes[0]));
+                    CurrentTime = CurrentTime.AddSeconds(59);
+                    UpperBounds[i] = CurrentTime.ToString().Split(' ')[1];
+                }
+                if (showPostTime[i].Contains("hours"))
+                {
+                    String[] Hours = showPostTime[i].Split(' ');
+                    DateTime CurrentTime = Convert.ToDateTime(exactPostTime[i]);
+                    CurrentTime = CurrentTime.AddHours(Int32.Parse(Hours[0]));
+                    CurrentTime = CurrentTime.AddMinutes(59);
+                    CurrentTime = CurrentTime.AddSeconds(59);
+                    UpperBounds[i] = CurrentTime.ToString().Split(' ')[1];
+                }
+            }
+            return UpperBounds;
+        }
+        //single handles the case when there is only one element in exactposttime 
+        //or if all the elements in exactposttime and showposttime are exactly same
+        public static string singleIndex(String exactPostTime, string showPostTime)
+        {
+            if (showPostTime.Contains("few"))
+            {
+                return exactPostTime;
+            }
+            if (showPostTime.Contains("minutes"))
+            {
+                String[] Minutes = showPostTime.Split(' ');
+                DateTime CurrentTime = Convert.ToDateTime(exactPostTime);
+                CurrentTime = CurrentTime.AddMinutes(Int32.Parse(Minutes[0]));
+                string answer = CurrentTime.ToString().Split(' ')[1];
+                return answer;
+            }
+            if (showPostTime.Contains("hours"))
+            {
+                String[] Hours = showPostTime.Split(' ');
+                DateTime CurrentTime = Convert.ToDateTime(exactPostTime);
+                CurrentTime = CurrentTime.AddHours(Int32.Parse(Hours[0]));
+                string answer = CurrentTime.ToString().Split(' ')[1];
+                return answer;
+            }
+            return null;
+        }
+        //CheckIntervals simply checks if a given time is in between given interval
+        public static int CheckIntervals(string LowerBound, string CurrentCheckTime, string UpperBound)
+        {
+            String[] UpperBoundterms = UpperBound.Split(':');
+            int UpperBoundHour = Int32.Parse(UpperBoundterms[0]);
+            int UpperBoundMinute = Int32.Parse(UpperBoundterms[1]);
+            int UpperBoundSeconds = Int32.Parse(UpperBoundterms[2]);
+            if (UpperBoundMinute == 0)
+            {
+                UpperBoundMinute = 60;
+            }
+            if (UpperBoundSeconds == 0)
+            {
+                UpperBoundSeconds = 60;
+            }
+            UpperBound = UpperBoundHour + ":" + UpperBoundMinute + ':' + UpperBoundSeconds;
+            if(String.Compare(LowerBound,CurrentCheckTime)<=0 && String.Compare(CurrentCheckTime,UpperBound)<=0)
+            {
+                return 1;
+            }
+            return 0;
         }
     }
 }
