cd4c716fb41d913d0b8159f648da141ac22842cb
Aniket <asingla@tavisca.in>
2019-07-08 17:59:10 +0530

    Added Suggested Changes

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/ForumPostEasy.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
+{
+    public class ForumPostEasy
+    {
+        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
+        {
+            int resultHour = -1, resultMinute = -1, resultSecond = -1;
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                for (int j = 0; j < exactPostTime.Length; j++)
+                {
+                    if (exactPostTime[i].Equals(exactPostTime[j]) && !showPostTime[i].Equals(showPostTime[j]))
+                    {
+                        return "impossible";
+                    }
+                }
+            }
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                int[] myInts = Array.ConvertAll(exactPostTime[i].Split(':'), int.Parse);
+                int givenHour = myInts[0], givenMinute = myInts[1], givenSecond = myInts[2];
+                string[] showPostTimeArray = showPostTime[i].Split(separator: ' ');
+                int X = -1;
+                if (!showPostTimeArray[0].Equals("few"))
+                {
+                    X = int.Parse(showPostTimeArray[0]);
+                }
+
+                string unit = showPostTimeArray[1];
+                if (unit.Equals("seconds"))
+                {
+                    resultHour = givenHour;
+
+                    if (resultSecond == -1)
+                    {
+                        resultMinute = givenMinute;
+                        resultSecond = givenSecond;
+                    }
+                    else if (givenMinute == resultMinute + 1 || givenMinute == resultMinute - 1)
+                    {
+                        resultMinute = givenMinute;
+                        resultSecond = givenSecond;
+                    }
+                    //Console.Write(resultHour + ":" + resultMinute + ":" + resultSecond+ "\n");
+                }
+                else if (unit.Equals("minutes"))
+                {
+                    if (givenHour == 23 && givenMinute + X >= 60)
+                    {
+                        resultHour = 00;
+                        resultMinute = (givenMinute + X) % 60;
+                        if (resultSecond == -1)
+                        {
+                            resultSecond = givenSecond;
+                        }
+                    }
+                    else if (givenMinute + X >= 60)
+                    {
+                        resultHour = givenHour + 1;
+                        resultMinute = (givenMinute + X) % 60;
+                        resultSecond = givenSecond;
+
+                    }
+                    else
+                    {
+                        resultHour = givenHour;
+                        resultMinute = givenMinute + X;
+
+                        resultSecond = givenSecond;
+                    }
+                    //Console.Write(resultHour + ":" + resultMinute + ":" + resultSecond+"\n");
+                }
+                else
+                {
+                    resultHour = (givenHour + X) % 24;
+                    if (resultMinute == -1)
+                    {
+                        resultMinute = givenMinute;
+                        resultSecond = givenSecond;
+                    }
+                    //Console.Write(resultHour + ":" + resultMinute + ":" + resultSecond+"\n");
+                }
+            }
+            string[] arr = new string[3];
+            arr[0] = resultHour.ToString();
+            arr[1] = resultMinute.ToString();
+            arr[2] = resultSecond.ToString();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (int.Parse(arr[i]) < 10)
+                {
+                    arr[i] = "0" + arr[i];
+                }
+            }
+            //Console.Write(string.Join(':', arr));
+            return string.Join(':', arr);
+
+
+        }
+
+    }
+}
--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -1,122 +1,25 @@
 ﻿using System;
 
-namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise2
 {
     public static class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] argivenSecond)
         {
             Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
-            Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
-            Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
+            Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 houresultSecond ago" }, "impossible");
+            Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 houresultSecond ago" }, "11:59:23");
             Console.ReadKey(true);
         }
 
         private static void Test(string[] postTimes, string[] showTimes, string expected)
         {
-            var result = GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
+            var result = ForumPostEasy.GetCurrentTime(postTimes, showTimes).Equals(expected) ? "PASS" : "FAIL";
             var postTimesCsv = string.Join(", ", postTimes);
             var showTimesCsv = string.Join(", ", showTimes);
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
-        public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
-        {
-            // Add your code here.
-            //throw new NotImplementedException();
-            int rh=-1, rm=-1, rs = -1;
-            for (int i = 0; i < exactPostTime.Length; i++)
-            {
-                for (int j = 0; j < exactPostTime.Length; j++)
-                {
-                    if(exactPostTime[i].Equals(exactPostTime[j])&& !showPostTime[i].Equals(showPostTime[j]))
-                    {
-                        return "impossible";
-                    }
-                }
-            }
-            for (int i = 0; i < exactPostTime.Length; i++)
-            {
-                int[] myInts = Array.ConvertAll(exactPostTime[i].Split(':'), int.Parse);
-                int gh = myInts[0], gm = myInts[1], gs = myInts[2];
-                string[] sp = showPostTime[i].Split(separator: ' ');
-                int X=-1;
-                if (!sp[0].Equals("few"))
-                {
-                    X = int.Parse(sp[0]);
-                }
-                
-                string unit = sp[1];
-                if (unit.Equals("seconds"))
-                {
-                    rh = gh;
-                   
-                    if (rs == -1)
-                    {
-                        rm = gm;
-                        rs = gs;
-                    }
-                    else if(gm == rm+1 || gm ==rm - 1)
-                    {
-                        rm = gm;
-                        rs = gs;
-                    }
-                    //Console.Write(rh + ":" + rm + ":" + rs+ "\n");
-                }
-                else if (unit.Equals("minutes"))
-                {
-                    if (gh == 23 && gm+X>=60)
-                    {
-                        rh = 00;
-                        rm = (gm + X) % 60;
-                        if (rs == -1)
-                        {
-                            rs = gs;
-                        }
-                    }
-                    else if(gm+X>=60)
-                    {
-                        rh = gh + 1;
-                        rm = (gm + X) % 60;
-                        rs = gs;
-
-                    }
-                    else
-                    {
-                        rh = gh;
-                        rm = gm + X;
-                       
-                        rs = gs;
-                    }
-                    //Console.Write(rh + ":" + rm + ":" + rs+"\n");
-                }
-                else
-                {
-                    rh = (gh + X) % 24;
-                    if (rm == -1)
-                    {
-                        rm = gm;
-                        rs = gs;
-                    }
-                    //Console.Write(rh + ":" + rm + ":" + rs+"\n");
-                }
-            }
-            string[] arr = new string[3];
-            arr[0] = rh.ToString();
-            arr[1] = rm.ToString();
-            arr[2] = rs.ToString();
-            for(int i=0;i<arr.Length;i++)
-            {
-                if (int.Parse(arr[i])<10)
-                {
-                    arr[i] = "0"+arr[i];
-                }
-            }
-            //Console.Write(string.Join(':', arr));
-            return string.Join(':', arr);
-
-
-        }
     }
 }
