c7826552b6c4a12a6903a99a747aa66edd0f5030
tavisca-cprasad <51946416+tavisca-cprasad@users.noreply.github.com>
2019-07-10 15:48:36 +0530

    Updated code into multiple functions 01

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -25,35 +25,20 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             const string IMPOSSIBLE = "impossible";
             int totaltime = exactPostTime.Length;
 
-            for(var i=0; i<totaltime; i++){
-                for(var j=0; j<totaltime; j++){
-                    if(i==j) continue;
-                    if(exactPostTime[i] == exactPostTime[j] && showPostTime[i] != showPostTime[j])
-                        return IMPOSSIBLE;
-                }
-            }
+            if(!isPossible(exactPostTime, showPostTime)) return IMPOSSIBLE;
 
             string[] currentTime = new string[totaltime];
 
             for(var i=0; i<totaltime; i++){
-                int hours = Convert.ToInt32(exactPostTime[i].Split(":")[0]);
-                int minutes = Convert.ToInt32(exactPostTime[i].Split(":")[1]);
-                int seconds = Convert.ToInt32(exactPostTime[i].Split(":")[2]);
-
-                DateTime timeAndDate = new DateTime(1900, 1, 1, hours, minutes, seconds);
+                int hours=0, minutes=0, seconds=0;
+                getHoursMinutesSeconds(exactPostTime[i], ref hours, ref minutes, ref seconds);
 
                 if(showPostTime[i].Contains("seconds"))
                     currentTime[i] = exactPostTime[i];
-                else if(showPostTime[i].Contains("minutes")){
-                    int showPostTimeMinutes = Convert.ToInt32(showPostTime[i].Split(" ")[0]);
-                    TimeSpan timeSpan = new TimeSpan(0, showPostTimeMinutes, 0);
-                    currentTime[i] = timeAndDate.Add(timeSpan).ToString().Split(" ")[1];
-                }
-                else if(showPostTime[i].Contains("hours")){
-                    int showPostTimeHours = Convert.ToInt32(showPostTime[i].Split(" ")[0]);
-                    TimeSpan timeSpan = new TimeSpan(showPostTimeHours ,0, 0);
-                    currentTime[i] = timeAndDate.Add(timeSpan).ToString().Split(" ")[1];
-                }
+                else if(showPostTime[i].Contains("minutes"))
+                    currentTime[i] = currentTimeAddMinutes(showPostTime[i], hours, minutes, seconds);
+                else if(showPostTime[i].Contains("hours"))
+                    currentTime[i] = currentTimeAddHours(showPostTime[i], hours, minutes, seconds);
             }
 
             Array.Sort(currentTime);
@@ -66,6 +51,37 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 Console.WriteLine("Input of length : 0");
                 return IMPOSSIBLE;
             }
+        }
+
+        public static bool isPossible(string[] exactPostTime, string[] showPostTime){
+            for(var i=0; i<exactPostTime.Length; i++){
+                for(var j=0; j<exactPostTime.Length; j++){
+                    if(i==j) continue;
+                    if(exactPostTime[i] == exactPostTime[j] && showPostTime[i] != showPostTime[j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static void getHoursMinutesSeconds(string exactPostTime, ref int hours, ref int minutes, ref int seconds){
+            hours = Convert.ToInt32(exactPostTime.Split(":")[0]);
+            minutes = Convert.ToInt32(exactPostTime.Split(":")[1]);
+            seconds = Convert.ToInt32(exactPostTime.Split(":")[2]);
+        }
+
+        public static string currentTimeAddMinutes(string showPostTime, int hours, int minutes, int seconds){
+            DateTime timeAndDate = new DateTime(1900, 1, 1, hours, minutes, seconds);
+            int showPostTimeMinutes = Convert.ToInt32(showPostTime.Split(" ")[0]);
+            TimeSpan timeSpan = new TimeSpan(0, showPostTimeMinutes, 0);
+            return timeAndDate.Add(timeSpan).ToString().Split(" ")[1];
+        }
+
+        public static string currentTimeAddHours(string showPostTime, int hours, int minutes, int seconds){
+            DateTime timeAndDate = new DateTime(1900, 1, 1, hours, minutes, seconds);
+            int showPostTimeHours = Convert.ToInt32(showPostTime.Split(" ")[0]);
+            TimeSpan timeSpan = new TimeSpan(showPostTimeHours, 0, 0);
+            return timeAndDate.Add(timeSpan).ToString().Split(" ")[1];
         }
     }
 }
